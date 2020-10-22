f229d92d6926d22ea17949d806c2fc645a574c85
Ly Ho Van <lyhv@tech.est-rouge.com>
2020-10-22 14:24:04 +0700

    first commit

---
+++ app/src/main/java/com/ervn/kisokmodesample/KioskAdminReceiver.kt
@@ -0,0 +1,35 @@
+package com.ervn.kisokmodesample
+
+import android.app.admin.DeviceAdminReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+import androidx.core.os.bundleOf
+
+class KioskAdminReceiver : DeviceAdminReceiver() {
+    override fun onEnabled(context: Context, intent: Intent) {
+        Toast.makeText(
+            context,
+            context.getString(R.string.device_admin_enabled),
+            Toast.LENGTH_SHORT
+        ).show()
+
+        context.sendBroadcast(Intent().apply {
+            action = "KioskAdminReceiver"
+            putExtra("KioskAdminReceiver", "KioskAdminReceiver")
+        })
+
+    }
+
+    override fun onDisabled(context: Context, intent: Intent) {
+        Toast.makeText(
+            context,
+            context.getString(R.string.device_admin_disabled),
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+
+    override fun onDisableRequested(context: Context, intent: Intent): CharSequence? {
+        return context.getString(R.string.device_admin_warning)
+    }
+}
